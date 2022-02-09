d871cac73de6a6847459e9c5bad26662e51d24d6
DestinyW <dwalcot1@binghamton.edu>
2022-02-09 12:21:06 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    pygame.init()
+    team = {
+        "Team Member 1": "Destiny Walcott", 
+        "Team Member 2": "Liz Voroshylo",
+        "Team Member 3": "Cole Cipp",
+    }
+main()
