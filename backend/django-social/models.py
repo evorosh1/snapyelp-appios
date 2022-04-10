ee7bbfd21d056c1cdfdc73d515c11fb52e45bb0e
colecipp <42648284+colecipp@users.noreply.github.com>
2022-04-10 17:06:15 -0400

    Add files via upload

--- backend/django-social/models.py
+++ backend/django-social/models.py
@@ -1,87 +1,77 @@
-from peewee import (
-    CharField,
-    DateTimeField,
-    DeferredForeignKey,
-    ForeignKeyField,
-    SqliteDatabase,
-    Model,
-    TextField,
-)
+from django.db import models
+from datetime import date
+from django.apps import AppConfig
+from requests import Response
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from rest_framework import status
+from django.contrib.auth.hashers import make_password
 
-import datetime
-import secrets
-# import settings
-import hashlib
+# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=100)
+    email = models.EmailField()
+    Bio = models.TextField()
+    #Friends
+    #profilePic = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, **options)
+    #time = models.DateTimeField(auto_now_add=True)
+    #public = models.BinaryField()
+    def __str__(self):
+        return self.username
+        
+class Post(models.Model):
+    Title = models.CharField(max_length=50)
+    Description = models.TextField()
+    URL = models.SlugField(max_length=40)
+    #profilePic = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100, **options)
+    time = models.DateTimeField(auto_now_add=True)
+    #user
 
-db = SqliteDatabase("./data/app.db", pragmas={"journal_mode": "wal"})
-
-
-class BaseModel(Model):
-    class Meta:
-        database = db
-        SCHEMA_VERSION = 0
-
-
-class User(BaseModel):
-    name = CharField(unique=True, index=True)
-    password = TextField(default="")
-
-    @staticmethod
-    def _hash_password(password):
-        return hashlib.scrypt(
-            bytes(password, encoding="utf8"), salt=settings.SITE_KEY, n=16384, r=8, p=1
-        ).hex()
-
-    def set_password(self, password):
-        self.password = User._hash_password(password)
-        self.save()
-
-    def generate_token(self):
-        new_token = Token.create(user=self)
-        return new_token
-
-    @staticmethod
-    def get_user_from_token(request):
-
-        token = request.cookies.token
-        if not token:
-            return None
+class Register(APIView):
+     def post(self, request):
+        # The order of required params is important as they are used to set variables by index
+        required_params = ['username', 'password', 'email']
         try:
-            token = Token.get(id=token)
-        except Token.DoesNotExist:
-            return None
+            data = request.data
+            # Checking if all the required parameters are available in data
+            if all(key in data for key in required_params):
+                try:
+                    user_name = self.validate_required_input(required_params[0], data[required_params[0]])
+                    password = self.validate_required_input(required_params[1], data[required_params[1]])
+                    email = self.validate_required_input(required_params[2], data[required_params[2]])
+                except ValidationError as er:
+                    return Response({"error": str(er.messages[0])}, status=status.HTTP_400_BAD_REQUEST)
 
-        if token.expires <= datetime.datetime.now():
-            token.delete_instance()
-            return None
+                # Input is now considered valid
+                # Creating user object to store to DB
+                new_user = User()
+                new_user.username = user_name
+                new_user.password = make_password(password)
+                new_user.email = email
 
-        return token.user
+                # Trying to set optional parameters if available
+                try:
+                    new_user.first_name = data['firstname'] if data['firstname'] is not None else ""
+                except KeyError:
+                    print("Error while parsing firstname ")
+                try:
+                    new_user.last_name = data['lastname'] if data['lastname'] is not None else ""
+                except KeyError:
+                    print("Error while parsing lastname")
 
-    #establishes msg account data structure
-class Msg(BaseModel):
-    user = ForeignKeyField(User, backref='msgs')
-    #reference to user
-    message = TextField()
-    #message
-    date = DateTimeField(default = datetime.datetime.now)
-    #date posted
-    reply_to = DeferredForeignKey("Msg", null=True)
+                new_user.save()
 
-    msg_route_prefix = "/msg"
-    @property
-    def link(self):
-        return f"{self.msg_route_prefix}/{self.id}"
+                return Response({"status": "Success"}, status=status.HTTP_201_CREATED)
 
-    @classmethod
-    def get_top_level_posts(cls):
-        return cls.select().where(cls.reply_to.is_null()).order_by(cls.date.desc())
-
-    def replies(self):
-        return Msg.select().where(Msg.reply_to == self).order_by(Msg.date.asc())        
-
-class Token(BaseModel):
-    id = TextField(primary_key=True, default=lambda: secrets.token_hex(32))
-    user = ForeignKeyField(User, backref="tokens")
-    expires = DateTimeField(
-        default=lambda: datetime.datetime.now() + datetime.timedelta(days=7), index=True
-    )
+            else:
+                return Response({"error": "Required param(s) missing, Please include and retry again"},
+                                status=status.HTTP_400_BAD_REQUEST)
+        except Exception as exp:
+            print("Unexpected exception occurred: "+str(exp))
+            return Response({"error": "Unexpected error occurred, please report this to Admin"},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
